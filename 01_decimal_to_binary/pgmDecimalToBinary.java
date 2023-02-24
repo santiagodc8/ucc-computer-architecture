7454ba494ea90480a46cbf05528aff877acce0f4
santiago duran <santidurancanedo@gmail.com>
2023-02-24 10:49:44 -0500

    complete decimal to binary

---
+++ 01_decimal_to_binary/pgmDecimalToBinary.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class pgmDecimalToBinary{
+public static void main(String[] args) {
+    Scanner obj = new Scanner(System.in);
+    int numero, residuo, binario=0,exp=0;
+    System.out.println("Escriba un numero : "); numero = obj.nextInt();
+    
+    //proceso
+    while(numero!=0){
+        residuo = numero%2;
+        binario+=residuo*Math.pow(10, exp);
+        exp++;
+        numero/=2;
+    }
+    System.out.println("El numero convertido a Binario es: " + binario);
+
+    }
+}
