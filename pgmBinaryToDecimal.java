ba3d57d0835b4adad4b128d0276abbe8b0dfb97b
santiago duran <santidurancanedo@gmail.com>
2023-02-28 20:18:47 -0500

    Add exception to ignored bin and class folders

--- pgmBinaryToDecimal.java
+++ pgmBinaryToDecimal.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
-public class pgmBinaryToDecimal{
+public class pgmBinaryToDecimal {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
-        int num, residuo,c=0,decimal=0;
+        int num, residuo, c = 0, decimal = 0;
         System.out.println("Escriba un numero binario: ");
         num = obj.nextInt();
 
-        //proceso
-        while(num!=0){
-            residuo=num%10;
-            decimal+=residuo*Math.pow(2, c);
+        // proceso
+        while (num != 0) {
+            residuo = num % 10;
+            decimal += residuo * Math.pow(2, c);
             c++;
-            num/=10;
+            num /= 10;
         }
         System.out.println("El numero convertido a decimal es: " + decimal);
     }
--- pgmDecimalToBinary.java
+++ pgmDecimalToBinary.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 
-public class pgmDecimalToBinary{
-public static void main(String[] args) {
-    Scanner obj = new Scanner(System.in);
-    int numero, residuo, binario=0,exp=0;
-    System.out.println("Escriba un numero entero : "); numero = obj.nextInt();
-    
-    //proceso
-    while(numero!=0){
-        residuo = numero%2;
-        binario+=residuo*Math.pow(10, exp);
-        exp++;
-        numero/=2;
-    }
-    System.out.println("El numero convertido a Binario es: " + binario);
+public class pgmDecimalToBinary {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int numero, residuo, binario = 0, exp = 0;
+        System.out.println("Escriba un numero entero : ");
+        numero = obj.nextInt();
+
+        // proceso
+        while (numero != 0) {
+            residuo = numero % 2;
+            binario += residuo * Math.pow(10, exp);
+            exp++;
+            numero /= 2;
+        }
+        System.out.println("El numero convertido a Binario es: " + binario);
 
     }
 }
